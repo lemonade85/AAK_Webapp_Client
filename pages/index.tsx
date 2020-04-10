edda6bdd727d4f0578e33318d5958e8d17f33965
Lenins George <lenins.george@finastra.com>
2020-04-10 14:23:41 +0530

    Initial scaffold with Next, React, Typescript, Yarn and Eslint configured

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+export default function Index() {
+    return (
+        <Layout>
+            <div>
+                <p>Hello Next.js</p>
+            </div>
+        </Layout>
+    );
+}
